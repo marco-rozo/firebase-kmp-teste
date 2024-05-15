f27997cdace2b932129def16a85244a6ca3f49af
marco-rozo <marcorozo99@gmail.com>
2024-05-14 23:57:00 -0300

    implements randomUUID using platform-specific APIs

--- composeApp/src/androidMain/kotlin/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/Platform.android.kt
@@ -1,7 +1,9 @@
 import android.os.Build
+import java.util.*
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+actual fun randomUUID() = UUID.randomUUID().toString()
--- composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -1,7 +1,9 @@
 import platform.UIKit.UIDevice
+import platform.Foundation.NSUUID
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+actual fun randomUUID(): String = NSUUID().UUIDString()
