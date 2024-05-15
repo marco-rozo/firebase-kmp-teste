28ef69569d19630c56b0a0492b236b331829bcf0
marco-rozo <marcorozo99@gmail.com>
2024-05-14 23:59:43 -0300

    using randomUUID and create fromString in category enum

--- composeApp/src/commonMain/kotlin/data/model/Expense.kt
+++ composeApp/src/commonMain/kotlin/data/model/Expense.kt
@@ -1,4 +1,4 @@
-package model
+package data.model
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Coffee
@@ -10,11 +10,12 @@ import androidx.compose.material.icons.filled.House
 import androidx.compose.ui.graphics.vector.ImageVector
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import randomUUID
 
 @Serializable
 @SerialName("OwnedProject")
 data class Expense(
-    val id:Long = -1,
+    val id: String? = randomUUID(),
     val amount: Double,
     val category:ExpenseCategory,
     val description: String
@@ -30,5 +31,18 @@ enum class ExpenseCategory(val icon: ImageVector){
     CAR(Icons.Default.ElectricCar),
     HOUSE(Icons.Default.House),
     OTHER(Icons.Default.ViewCozy)
+
+    companion object {
+        fun fromString(category: String): ExpenseCategory {
+            return when (category.uppercase()) {
+                "PARTY" -> PARTY
+                "SNACKS" -> SNACKS
+                "COFFEE" -> COFFEE
+                "CAR" -> CAR
+                "HOUSE" -> HOUSE
+                else -> OTHER
+            }
+        }
+    }
 
 }
